c67bbbc12c8d89b5c8f7ef5951c2204d7f6fe34c
Cathal Redmond <cathalred88@hotmail.com>
2025-02-04 21:15:07 +0000

    added lab2.1.py file

--- pands-weeklytasks/Week 2/Lab 2.1.py
+++
@@ -1,6 +0,0 @@
-## this is a lab workbook to test basic functions
-# Author: Cathal Redmond
-
-A = (1 + 2) 
-print (a) 
-
---
+++ pands-weeklytasks/Week2/Lab2.1.py
@@ -0,0 +1,14 @@
+## this is a lab workbook to test basic functions
+# Author: Cathal Redmond
+
+# Code to assign a variable "a" the value contained in the brackets
+a = (1 * 2) 
+
+# code to print the value of what is stored in the variable "a"
+print (a) 
+
+#check what type of data variable "a" is
+print (type(a))
+
+
+# remainder of lab completed in termial, not saved in text file. 
