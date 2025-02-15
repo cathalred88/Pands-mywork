76091f011634713456cd814089a748ccce35f84d
Cathal Redmond <cathalred88@hotmail.com>
2025-02-15 15:41:01 +0000

    update convert program

--- pands-weeklytasks/Week03/convert.py
+++ pands-weeklytasks/Week03/convert.py
@@ -3,12 +3,27 @@
 # Author Cathal Redmond 14 feb 2025 (Happy Valenines day)
 import math
 
-number = float(input("Please enter the number (in Dollars and cents): "))
-dollars = ((number%100)*100)
-absolutedollars = abs(dollars)
+signednumber = float(input("Please enter the number (in Dollars and cents): "))
+sign = str('positive') if signednumber > 0 else str('negative') #checks the sign of the number and saves it as a string. I making the decision that a bank would need to keep track if a transaction is positive or negative! 
+print ('Sign of input amount is : {} '.format(sign)) # prints the sign of the number
+cents = (signednumber * 100)
+number = int(abs(cents))
 
-cents = (number//100)
-absolutecents = abs(cents)
+#total_in_cents = (number*100)
+print ('The amount in cents is {}'.format(number))
 
-total = int(absolutedollars)+(absolutecents)
-print ('The amount in cents is {}'.format(total))
+
+
+
+# RIP all the different way i tried to force the numbers not to be inaccurate from floating point errors 
+
+#dollars = ((number%100)*100) # seperates the dollar part of the amount
+#print (dollars)
+#intdollars = int(dollars)
+#print(intdollars)
+#cents = (number//100) # seperates the cents part of the amount
+#intcents = int(cents)
+#print (cents)
+#print (intcents)
+
+#total = int(intdollars)+(intcents)
