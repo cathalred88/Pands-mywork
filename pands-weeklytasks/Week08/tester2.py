3cdbaaaebf9d12c3fc31b71fac5e80081d34b084
Cathal Redmond <cathalred88@hotmail.com>
2025-04-28 22:29:04 +0100

    finished plottask.py - had to reformat the program entirely as my axis were not displaying correctly.

--- pands-weeklytasks/Week08/tester2.py
+++ pands-weeklytasks/Week08/tester2.py
@@ -1,22 +1,20 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-line = [0.2, 0.3, 0.37, 0.4, 0.6, 0.7, 0.72, 0.75, 0.77, 0.78, 0.79, 0.795]
-distribution = [0.2, 0.3, 0.3, 0.4, 0.7, 0.7, 0.7, 0.8]
+from mpl_toolkits.axes_grid1 import host_subplot
 
-fig, ax = plt.subplots(1, 1)
+host = host_subplot(111)
+par = host.twinx()
 
-ax2 = ax.twinx()
-ax3 = ax2.twiny()
+host.set_xlabel("Distance")
+host.set_ylabel("Density")
+par.set_ylabel("Temperature")
 
-ax.plot(line)
-ax.set_xlabel('line xaxis')
-ax.set_ylabel('line yaxis')
+p1, = host.plot([0, 1, 2], [0, 1, 2], label="Density")
+p2, = par.plot([0, 1, 2], [0, 3, 2], label="Temperature")
 
-ax3.hist(distribution, orientation='horizontal', alpha=.5)
-ax3.invert_xaxis()
-ax3.set_xlabel('hist xaxis, note where 0 is')
-# note this needs to be ax2 due to subtle overlay issues....
-ax2.set_ylabel('hist yaxis')
-plt.draw()
+host.legend(labelcolor="linecolor")
+
+host.yaxis.label.set_color(p1.get_color())
+par.yaxis.label.set_color(p2.get_color())
+
 plt.show()
--- pands-weeklytasks/Week08/tester3.py
+++ pands-weeklytasks/Week08/tester3.py
@@ -1,23 +1,3 @@
-#plottask.py
-# script for plotting histograms and a funstion on same axis 
-# Author Cathal Redmond 26 Apr 2025
-'''
-Instructions: 
-Write a program called plottask.py that displays:
-
-a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
-and a plot of the function  h(x)=x3 in the range 0 to 10, 
-on the one set of axes.
-Some marks will be given for making the plot look nice (legend etc).
-
-Please put a copy of the image of the plot (.png file) into the repository
-'''
-#References
-# https://www.w3schools.com/python/ref_func_pow.asp
-# https://stackoverflow.com/questions/14762181/adding-a-y-axis-label-to-secondary-y-axis-in-matplotlib
-# https://stackoverflow.com/questions/9622163/save-plot-to-image-file-instead-of-displaying-it
-
-# imports
 import matplotlib.pyplot as pt
 import numpy as np
 
